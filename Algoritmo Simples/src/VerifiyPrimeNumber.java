729a91d5f2e01af347d3e772302aeecf299cc801
Vinicius Petris <viniciusbosa@gmail.com>
2021-06-19 17:05:54 -0300

    algoritmo simples e lista encadeada adicionado

---
+++ Algoritmo Simples/src/VerifiyPrimeNumber.java
@@ -0,0 +1,22 @@
+public class VerifiyPrimeNumber {
+    public static int verify(int number) {
+        int i, m = 0, flag = 0;
+        int n = number;
+        int r = 0;
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            r = 0;
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    flag = 1;
+                    r = 0;
+                }
+            }
+            if (flag == 0) {
+                r = 1;
+            }
+        }
+        return r;
+    }
+}
