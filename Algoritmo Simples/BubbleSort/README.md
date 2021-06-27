54efe8b85266ed457ef58fb4da569c317f4b398e
Vinicius Petris <viniciusbosa@gmail.com>
2021-06-27 12:50:35 -0300

    testes e readme do bubble sort adicionado

---
+++ Algoritmo Simples/BubbleSort/README.md
@@ -0,0 +1,9 @@
+O código representa a implementação de ordenação de um vetor de numeros utilizando o algoritmo BubbleSort
+
+Para instanciar a classe utiliza-se a chamada BubbleSort(array) passando por parâmetro um array de numero inteiros;
+
+
+A classe possui os seguintes métodos:
+
+- ordena(): ordena o vetor na ordem crescente;
+- toString(): printa o vetor;
--- Algoritmo Simples/BubbleSort/src/BubbleSort.java
+++ Algoritmo Simples/BubbleSort/src/BubbleSort.java
@@ -1,16 +1,32 @@
+import java.util.Arrays;
+
 public class BubbleSort {
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
+
+    private int arr[];
+
+    public BubbleSort(int array[]) {
+        arr = array;
+    }
+
+    public void ordena() {
+        int n = this.arr.length;
         int temp = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+                if (this.arr[j - 1] > this.arr[j]) {
+                    temp = this.arr[j - 1];
+                    this.arr[j - 1] = this.arr[j];
+                    this.arr[j] = temp;
                 }
 
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "BubbleSort{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
     }
 }
