3640a48295a5b52619c4e9da987099787fe3625e
Vinicius Petris <vinicius.petris@b2wdigital.com>
2021-07-16 00:11:12 -0300

    adicionado novos teste a Fila Circular, atingindo 100% de cobertura

--- fila-circular/src/main/java/CircularQueue.java
+++ fila-circular/src/main/java/CircularQueue.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 
 public class CircularQueue {
-    private int size, front, rear;
-    private ArrayList<Integer> queue = new ArrayList<Integer>();
+    private final int size;
+    private int front;
+    private int rear;
+    private final ArrayList<Integer> queue = new ArrayList<Integer>();
 
     CircularQueue(int size) {
         this.size = size;
@@ -10,40 +12,31 @@ public class CircularQueue {
     }
 
     // Inseri um novo elemento na fila.
-    public void enQueue(int data) {
+    public boolean enQueue(int data) {
 
-        if (!isFull()) ;
+        isFull();
 
         if (isEmpty()) {
             front = 0;
             rear = 0;
             queue.add(rear, data);
-        } else if (rear == size - 1 && front != 0) {
-            rear = 0;
-            queue.set(rear, data);
         } else {
             rear = (rear + 1);
-
-            if (front <= rear) {
-                queue.add(rear, data);
-            } else {
-                queue.set(rear, data);
+            queue.add(rear, data);
             }
-        }
+        return true;
     }
 
     // Remove um elemento na fila
     public int deQueue() {
         int temp;
 
-        if (isEmpty()) ;
+        isEmpty();
         temp = queue.get(front);
 
         if (front == rear) {
             front = -1;
             rear = -1;
-        } else if (front == size - 1) {
-            front = 0;
         } else {
             front = front + 1;
         }
@@ -52,18 +45,12 @@ public class CircularQueue {
     }
 
     public boolean isFull() {
-        if ((front == 0 && rear == size - 1) ||
-                (rear == (front - 1) % (size - 1))) {
-            return true;
-        }
-        return false;
+        return (front == 0 && rear == size - 1) ||
+                (rear == (front - 1) % (size - 1));
     }
 
     public boolean isEmpty() {
-        if (front == -1) {
-            return true;
-        }
-        return false;
+        return front == -1;
     }
 
     public int get(int i) {
