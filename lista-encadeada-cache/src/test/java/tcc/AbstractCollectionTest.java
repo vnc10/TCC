d1cae2fb16e309ef82ad30df59f9cbd44c3a28d7
Vinicius Petris <vinicius.petris@b2wdigital.com>
2021-08-18 10:56:29 -0300

    removido o target de todos os projetos

--- lista-encadeada-cache/src/test/java/tcc/AbstractCollectionTest.java
+++ lista-encadeada-cache/src/test/java/tcc/AbstractCollectionTest.java
@@ -1,3 +1,4 @@
+package tcc;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -14,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package tcc;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
