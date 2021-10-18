2005506296390693166e3198412f80be89d2c106
Vinicius Petris <vinicius.petris@b2wdigital.com>
2021-10-18 18:01:45 -0300

    add a new test on bubble sort

--- bubblesort/src/test/java/tcc/BubbleSortTest.java
+++ bubblesort/src/test/java/tcc/BubbleSortTest.java
@@ -1,7 +1,7 @@
 package tcc;
+
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
 public class BubbleSortTest {
@@ -10,8 +10,7 @@ public class BubbleSortTest {
         int[] array = {3, 60, 35, 2, 45, 320, 5};
         int[] ordenado = {2, 3, 5, 35, 45, 60, 320};
         BubbleSort bubble = new BubbleSort(array);
-        bubble.ordena();
-        assertArrayEquals(bubble.ordena(), ordenado);
+        assertArrayEquals(ordenado, bubble.ordena());
 //        for (int i = 0; i < array.length; i++) {
 //            assertEquals(ordenado[i], array[i]);
 //        }
@@ -21,7 +20,6 @@ public class BubbleSortTest {
     public void testShouldDoNothingWithOneElementArray() throws Throwable {
         int[] array = {42};
         BubbleSort bubble = new BubbleSort(array);
-        bubble.ordena();
-        assertArrayEquals(new int[]{42}, array);
+        assertArrayEquals(new int[]{42}, bubble.ordena());
     }
 }
